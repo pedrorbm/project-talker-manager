d0913d53935c79c2bc6bbbde7aec9069de66e435
Pedro Medeiros <pedropwks@gmail.com>
2023-06-21 16:46:47 -0300

    Adiciona requisito 4 - Adicione as validações para o endpoint /login

---
+++ src/middlewares/validatingLogin.js
@@ -0,0 +1,45 @@
+const validatingKeyEmail = (req, res, next) => {
+  const keyEmail = ['email'];
+  if (keyEmail.every((key) => key in req.body)) {
+    next();
+  } else {
+    res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+};
+
+const validatingEmail = (req, res, next) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const { email } = req.body;
+
+  if (regex.test(email)) {
+    next();
+  } else {
+    res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+};
+
+const validatingKeyPassword = (req, res, next) => {
+  const keyPassword = ['password'];
+  if (keyPassword.every((key) => key in req.body)) {
+    next();
+  } else {
+    res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+};
+
+const validatingPassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (password.length >= 6) {
+    next();
+  } else {
+    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+};
+
+module.exports = {
+  validatingKeyEmail,
+  validatingEmail,
+  validatingKeyPassword,
+  validatingPassword,
+};
--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const createToken = require('../middlewares/createToken');
+const { validatingKeyEmail, validatingEmail, validatingKeyPassword,
+  validatingPassword } = require('../middlewares/validatingLogin');
 
 const router = express.Router();
+router.use(express.json());
 
-router.post('/login', (req, res) => {
+router.post('/login', validatingKeyEmail, validatingEmail, validatingKeyPassword, 
+validatingPassword, (req, res) => {
   const token = createToken();
   res.status(200).json({ token: `${token}` });
 });
