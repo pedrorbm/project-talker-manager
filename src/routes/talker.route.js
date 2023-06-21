0b20e3567cf90371b15366196f30c82c2d44508d
Pedro Medeiros <pedropwks@gmail.com>
2023-06-21 02:17:38 -0300

    Adiciona requisito 2 - Crie o endpoint GET /talker/:id

--- src/routes/talker.route.js
+++ src/routes/talker.route.js
@@ -8,4 +8,15 @@ router.get('/talker', async (req, res) => {
   res.status(200).json(talkers);
 });
 
+router.get('/talker/:id', async (req, res) => {
+  const talkers = await talkersRead();
+  const { id } = req.params;
+  const searchId = talkers.find((talker) => talker.id === Number(id));
+
+  if (!searchId) {
+    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  } 
+  return res.status(200).json(searchId);
+});
+
 module.exports = router;
